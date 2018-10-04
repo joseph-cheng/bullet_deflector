57e784f361aec4e6c4c0e9d5c7bddafbe65cbb8b
pomchee2 <joe.cheng2001@gmail.com>
2018-10-04 11:29:24 +0100

    added deflector to player

--- src/player.py
+++ src/player.py
@@ -8,6 +8,14 @@ class Player:
         self.vel = Vec2(0, 0)
 
 
+        # Angle the deflector is facing
+        self.deflector_angle = 0
+
+        # Width of deflector
+        self.deflector_width = 10
+        
+        #Distance the delfector is from the centre of player
+        self.deflector_distance_from_player = 15
 
         # collisions are done using circles, so we store the radius of the player
         self.radius = 10
@@ -21,6 +29,15 @@ class Player:
         pygame.draw.circle(screen, (0,0,0), self.pos.to_tuple(), self.radius)
 
 
+        deflector_mid_point = self.pos + Vec2(self.deflector_distance_from_player*math.cos(self.deflector_angle),
+                                              self.deflector_distance_from_player*math.sin(self.deflector_angle))
+
+        deflector_start = deflector_mid_point + Vec2(-(self.deflector_width/2)*math.cos(self.deflector_angle),
+                                                     -(self.deflector_width/2)*math.sin(self.deflector_angle))
+        deflector_end = deflector_mid_point + Vec2((self.deflector_width/2)*math.sin(self.deflector_angle),
+                                                   (self.deflector_width/2)*math.cos(self.deflector_angle))
+
+        pygame.draw.line(screen, (0,0,0), deflector_start.to_tuple(), deflector_end.to_tuple())
 
 
 
